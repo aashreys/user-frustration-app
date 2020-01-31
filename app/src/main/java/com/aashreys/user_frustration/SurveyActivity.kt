949583669953c98b050ed79164c343edaa0c9c63
aashreys <aashrey9sharma@gmail.com>
2020-01-31 09:18:21 -0500

    Added activity layouts

---
+++ app/src/main/java/com/aashreys/user_frustration/SurveyActivity.kt
@@ -0,0 +1,12 @@
+package com.aashreys.user_frustration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SurveyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_survey)
+    }
+}
